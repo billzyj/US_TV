31335d315fbbb3275925ceae3c938260cf6ec46a
billzyj <billzyj2@gmail.com>
2025-02-18 16:58:04 -0600

    Update TV_Webscraping.py

---
+++ TV_Webscraping.py
@@ -0,0 +1,8 @@
+from src.FuboTV import scrape_fubo_tv
+from src.YoutubeTV import scrape_youtube_tv
+from src.SlingTV import scrape_sling_tv
+
+if __name__ == "__main__":
+    scrape_fubo_tv()
+    scrape_youtube_tv()
+    scrape_sling_tv()
